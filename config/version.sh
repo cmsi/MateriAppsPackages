23275f19dec94e108a842060d8d12505e4e7f907
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2022-12-25 09:41:40 +0900

    add support of bookworm

--- config/version.sh
+++ config/version.sh
@@ -1,7 +1,8 @@
-VERSION_bullseye="11.5"
+VERSION_bookworm="bookworm"
+VERSION_bullseye="11.6"
 VERSION_buster="10.13"
 VERSION_focal="20.04"
 VERSION_jammy="22.04"
 VERSION_bionic="18.04"
 
-VERSIONS="bullseye/debian:${VERSION_bullseye} buster/debian:${VERSION_buster} jammy/ubuntu:${VERSION_jammy} focal/ubuntu:${VERSION_focal} bionic/ubuntu:${VERSION_bionic}"
+VERSIONS="bookworm/debian:${VERSION_bookworm} bullseye/debian:${VERSION_bullseye} buster/debian:${VERSION_buster} jammy/ubuntu:${VERSION_jammy} focal/ubuntu:${VERSION_focal} bionic/ubuntu:${VERSION_bionic}"
