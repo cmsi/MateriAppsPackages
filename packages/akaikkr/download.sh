f4318181fe8fe21e119ef676b40a6924fbc24ce4
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2023-07-04 11:13:48 +0900

    [akaikkr] new upstream version 20230519

--- packages/akaikkr/download.sh
+++ packages/akaikkr/download.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
-. $(dirname $0)/path.sh
 
-if [ -f cpa2002v010.tgz ]; then :; else
-  echo "cpa2002v010.tgz not found"
+PACKAGE=akaikkr
+PACKAGE_DIR=$(cd $(dirname $0) && pwd)
+
+. ${PACKAGE_DIR}/../../script/path.sh
+
+if [ -f cpa2021v01.tgz ]; then :; else
+  echo "cpa2021v01.tgz not found"
   exit 127
 fi
 
-tar zxf cpa2002v010.tgz
-mv cpa2002v010 akaikkr_$VERSION_BASE
-COPYFILE_DISABLE=1 tar zcvf akaikkr_$VERSION_BASE.orig.tar.gz --exclude ".DS_Store" akaikkr_$VERSION_BASE
-rm -rf akaikkr_$VERSION_BASE
+tar zxf cpa2021v01.tgz
+mv cpa2021 ${PACKAGE}_$VERSION_BASE
+tar zcvf ${PACKAGE}_$VERSION_BASE.orig.tar.gz ".DS_Store" ${PACKAGE}_$VERSION_BASE
+rm -rf ${PACKAGE}_$VERSION_BASE
