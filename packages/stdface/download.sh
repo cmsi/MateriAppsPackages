5936f1a94073d20c490c147c7583ddd4d15670cf
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2023-07-17 11:31:43 +0900

    [stdface] Initial release for 0.5

---
+++ packages/stdface/download.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+PACKAGE=stdface
+PACKAGE_DIR=$(cd $(dirname $0) && pwd)
+
+. ${PACKAGE_DIR}/../../script/path.sh
+
+if [ -f ${PACKAGE}_$VERSION_BASE.orig.tar.gz ]; then
+  exit 127
+fi
+wget https://github.com/issp-center-dev/StdFace/archive/refs/tags/v0.5.tar.gz
+mkdir ${PACKAGE}-${VERSION_BASE}
+tar zxvf v${VERSION_BASE}.tar.gz -C ${PACKAGE}-${VERSION_BASE} --strip-component=1
+tar zcvf ${PACKAGE}_${VERSION_BASE}.orig.tar.gz ${PACKAGE}-${VERSION_BASE}
+rm -rf ${PACKAGE}-${VERSION_BASE} v${VERSION_BASE}.tar.gz
