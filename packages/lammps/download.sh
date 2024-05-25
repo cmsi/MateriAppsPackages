49ef6469718f9dbff5f39429a5367cda134b7013
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2024-05-25 23:21:26 +0900

    [lammps] working

--- packages/lammps/download.sh
+++ packages/lammps/download.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 
-VERSION=$(head -1 debian/changelog | sed 's/1://g' | cut -d ' ' -f 2 | sed 's/[()]//g')
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+VERSION=$(head -1 ${SCRIPT_DIR}/debian/changelog | sed 's/1://g' | cut -d ' ' -f 2 | sed 's/[()]//g')
 VERSION_BASE=$(echo ${VERSION} | cut -d '-' -f 1)
 
 echo ${VERSION}
 echo ${VERSION_BASE}
 
-if [ -f lammps-${VERSION_BASE}.tar.gz ]; then
+if [ ${VERSION_BASE} = "20230802" ]; then
+  VERSION_DOWNLOAD="2Aug2023-update3"
+  VERSION_DOWNLOAD_DIR="stable_2Aug2023_update3"
+fi
+
+echo ${VERSION_DOWNLOAD}
+echo ${VERSION_DOWNLOAD_DIR}
+
+if [ -f lammps-${VERSION_BASE}.orig.tar.gz ]; then
   exit 127
 fi
 
-wget http://deb.debian.org/debian/pool/main/l/lammps/lammps_${VERSION_BASE}.orig.tar.xz
-xz -d lammps_${VERSION_BASE}.orig.tar.xz
-gzip lammps_${VERSION_BASE}.orig.tar
+rm -f lammps-src-${VERSION_DOWNLOAD}.tar.gz
+wget https://github.com/lammps/lammps/releases/download/${VERSION_DOWNLOAD_DIR}/lammps-src-${VERSION_DOWNLOAD}.tar.gz
+mkdir lammps-${VERSION_BASE}
+tar -xvf lammps-src-${VERSION_DOWNLOAD}.tar.gz -C lammps-${VERSION_BASE} --strip-components=1
+tar -cvzf lammps-${VERSION_BASE}.orig.tar.gz lammps-${VERSION_BASE}
+rm -rf lammps-src-${VERSION_DOWNLOAD}.tar.gz lammps-${VERSION_BASE}
