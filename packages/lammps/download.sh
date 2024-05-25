6a0c6d49e334f9ff2c1d63495680e55182fba297
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2024-05-25 09:52:42 +0900

    [lammps] 20240207+dfsg-1.1build4

--- packages/lammps/download.sh
+++ packages/lammps/download.sh
@@ -3,12 +3,13 @@
 VERSION=$(head -1 debian/changelog | sed 's/1://g' | cut -d ' ' -f 2 | sed 's/[()]//g')
 VERSION_BASE=$(echo ${VERSION} | cut -d '-' -f 1)
 
-if [ -f hphi-${VERSION_BASE}.orig.tar.gz ]; then
+echo ${VERSION}
+echo ${VERSION_BASE}
+
+if [ -f lammps-${VERSION_BASE}.tar.gz ]; then
   exit 127
 fi
 
-wget https://github.com/issp-center-dev/HPhi/releases/download/v${VERSION_BASE}/HPhi-${VERSION_BASE}.tar.gz
-tar zxvf HPhi-${VERSION_BASE}.tar.gz
-mv -f HPhi-${VERSION_BASE} hphi-${VERSION_BASE}
-tar zcvf hphi_${VERSION_BASE}.orig.tar.gz hphi-${VERSION_BASE}
-rm -rf hphi-${VERSION_BASE} HPhi-${VERSION_BASE}.tar.gz
+wget http://deb.debian.org/debian/pool/main/l/lammps/lammps_${VERSION_BASE}.orig.tar.xz
+xz -d lammps_${VERSION_BASE}.orig.tar.xz
+gzip lammps_${VERSION_BASE}.orig.tar
