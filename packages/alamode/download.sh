2bde1ed56c617372352d28b3d1df0d29305f5522
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2024-07-29 15:43:08 +0900

    New upstream version 1.5.0

--- packages/alamode/download.sh
+++ packages/alamode/download.sh
@@ -5,17 +5,11 @@ PACKAGE_DIR=$(cd $(dirname $0) && pwd)
 
 . ${PACKAGE_DIR}/../../script/path.sh
 
-VERSION_DATE=
-if [ ${VERSION_BASE} = 1.4.2 ]; then
-  VERSION_DATE="2022-09-28"
-fi
-
 if [ -f ${PACKAGE}_${VERSION_BASE}.orig.tar.gz ]; then
   exit 127
 fi
 
-wget -O ${PACKAGE}-${VERSION_BASE}.tar.gz https://sourceforge.net/projects/${PACKAGE}/files/v.${VERSION_BASE}/ALAMODE%20version%20${VERSION_BASE}%20%28${VERSION_DATE}%29.tar.gz
-# wget -O alamode-${VERSION_BASE}.tar.gz https://sourceforge.net/projects/alamode/files/v.${VERSION_BASE}/ALAMODE%20version%20${VERSION_BASE}%20%28${VERSION_DATE}%29.tar.gz
+wget -O ${PACKAGE}-${VERSION_BASE}.tar.gz https://sourceforge.net/projects/alamode/files/v.1.5.0/v.1.5.0%20source%20code.tar.gz/download
 rm -rf ${PACKAGE}-${VERSION_BASE} && mkdir ${PACKAGE}-${VERSION_BASE}
 tar zxvf ${PACKAGE}-${VERSION_BASE}.tar.gz -C ${PACKAGE}-${VERSION_BASE} --strip-component=1
 tar zcvf ${PACKAGE}_${VERSION_BASE}.orig.tar.gz ${PACKAGE}-${VERSION_BASE}
