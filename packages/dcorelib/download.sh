9260de333fd99e2c21a5fc3ea84e034297d394a4
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2025-05-07 21:12:40 +0900

    [dcorelib] Initial release for 0.9.4

---
+++ packages/dcorelib/download.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+PACKAGE=dcorelib
+PACKAGE_DIR=$(cd "$(dirname "$0")" && pwd)
+
+# shellcheck disable=SC1091
+. "$PACKAGE_DIR"/../../script/path.sh
+
+if [ -f "$PACKAGE"_"$VERSION_BASE".orig.tar.gz ]; then
+  exit 127
+fi
+rm -rf "$PACKAGE"-"$VERSION_BASE"
+mkdir -p "$PACKAGE"-"$VERSION_BASE"
+wget -O - https://github.com/shinaoka/dcorelib/archive/refs/tags/v"$VERSION_BASE".tar.gz | tar zxvf - -C "$PACKAGE"-"$VERSION_BASE" --strip-components=1
+tar zcvf "$PACKAGE"_"$VERSION_BASE".orig.tar.gz "$PACKAGE"-"$VERSION_BASE"
+rm -rf "$PACKAGE"-"$VERSION_BASE"
