bd311a9bbb8b0f89d90c8c62b24e7869d7ab1d0e
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2025-08-22 12:08:35 +0900

    [akaikkr] New upstream version 20250731

--- packages/akaikkr/download.sh
+++ packages/akaikkr/download.sh
@@ -5,12 +5,12 @@ PACKAGE_DIR=$(cd $(dirname $0) && pwd)
 
 . ${PACKAGE_DIR}/../../script/path.sh
 
-if [ -f cpa2021v01.tgz ]; then :; else
-  echo "cpa2021v01.tgz not found"
+if [ -f cpa2021v02.tgz ]; then :; else
+  echo "cpa2021v02.tgz not found"
   exit 127
 fi
 
-tar zxf cpa2021v01.tgz
-mv cpa2021 ${PACKAGE}_$VERSION_BASE
+tar zxf cpa2021v02.tgz
+mv cpa2021v02 ${PACKAGE}_$VERSION_BASE
 tar zcvf ${PACKAGE}_$VERSION_BASE.orig.tar.gz ${PACKAGE}_$VERSION_BASE
 rm -rf ${PACKAGE}_$VERSION_BASE
