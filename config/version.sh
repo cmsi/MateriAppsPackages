5dcfac855a098e1564a749e578e387653ee1bd64
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2025-08-22 09:12:02 +0900

    - Started support for Debian 14 (Forky) - Stopped support for Ubuntu 20.04 (Focal)

--- config/version.sh
+++ config/version.sh
@@ -1,8 +1,11 @@
-DATE_DEBIAN="20250428"
+#!/bin/bash
+
+DATE_DEBIAN="20250811"
+VERSION_forky="forky-${DATE_DEBIAN}"
 VERSION_trixie="trixie-${DATE_DEBIAN}"
 VERSION_bookworm="bookworm-${DATE_DEBIAN}"
 VERSION_bullseye="bullseye-${DATE_DEBIAN}"
-VERSION_noble="noble-20250415.1"
-VERSION_jammy="jammy-20250415.1"
+VERSION_noble="noble-20250716"
+VERSION_jammy="jammy-20250730"
 
-VERSIONS="trixie/debian:${VERSION_trixie} bookworm/debian:${VERSION_bookworm} bullseye/debian:${VERSION_bullseye} noble/ubuntu:${VERSION_noble} jammy/ubuntu:${VERSION_jammy}"
+VERSIONS="forky/debian:${VERSION_forky} trixie/debian:${VERSION_trixie} bookworm/debian:${VERSION_bookworm} bullseye/debian:${VERSION_bullseye} noble/ubuntu:${VERSION_noble} jammy/ubuntu:${VERSION_jammy}"
