a85f526daac4c06372c1a7657043ac64c5978d18
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2025-10-01 13:54:27 +0900

    update debian and ubuntu versions

--- config/version.sh
+++ config/version.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-DATE_DEBIAN="20250811"
+DATE_DEBIAN="20250929"
 VERSION_forky="forky-${DATE_DEBIAN}"
 VERSION_trixie="trixie-${DATE_DEBIAN}"
 VERSION_bookworm="bookworm-${DATE_DEBIAN}"
 VERSION_bullseye="bullseye-${DATE_DEBIAN}"
-VERSION_noble="noble-20250716"
-VERSION_jammy="jammy-20250730"
+VERSION_noble="noble-20250910"
+VERSION_jammy="jammy-20250819"
 
 VERSIONS="forky/debian:${VERSION_forky} trixie/debian:${VERSION_trixie} bookworm/debian:${VERSION_bookworm} bullseye/debian:${VERSION_bullseye} noble/ubuntu:${VERSION_noble} jammy/ubuntu:${VERSION_jammy}"
